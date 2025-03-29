8113c3849ec423a1bcb71d2366ca853ce16484ab
Miguel Ángel <Miguel ngel@LAPTOP-4HCGUP4R>
2025-03-29 13:04:03 +0100

    Primer cambio realizado en el proyecto

---
+++ src/paqnombre1nombre2/NewMain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paqnombre1nombre2;
+
+/**
+ *
+ * @author Miguel Ángel
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Marce.");
+    }
+
+}
